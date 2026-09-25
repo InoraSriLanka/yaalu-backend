29f15c192cd24cca33bef0b9df799c5fb509738a
S.A.C.Jayanga <chamidujayanga9@gmail.com>
2026-09-25 16:19:45 +0530

    realtime rider request and notification setup

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -30,6 +30,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port, '0.0.0.0');
